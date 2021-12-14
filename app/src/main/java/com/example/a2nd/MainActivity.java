08ea9532c12a17ab8beab45dfb470d9eae33c9a0
YujinKim221 <uj9786@gmail.com>
2021-12-14 21:12:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a2nd/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a2nd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+}
+
+
+
+
+
